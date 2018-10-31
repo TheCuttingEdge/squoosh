90895433b55bdcc83bb30a63ea63a1b482f13319
Jake Archibald <jaffathecake@gmail.com>
2018-10-31 17:44:36 +0000

    Moving SW plugin later so it picks up things like HTML

--- webpack.config.js
+++ webpack.config.js
@@ -191,8 +191,6 @@ module.exports = function (_, env) {
 
       new WorkerPlugin(),
 
-      new AutoSWPlugin(),
-
       // Automatically split code into async chunks.
       // See: https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
       isProd && new webpack.optimize.SplitChunksPlugin({}),
@@ -238,6 +236,8 @@ module.exports = function (_, env) {
         inject: true,
         compile: true
       }),
+
+      new AutoSWPlugin({}),
 
       new ScriptExtHtmlPlugin({
         defaultAttribute: 'async'
