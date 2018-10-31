d69710e5b13b0f586bb6e8cf2dc5a66bfb374974
Jason Miller <developit@google.com>
2018-10-31 00:37:16 -0400

    Fix contenthash issues. Courtesy of an updated mini-css-extract-plugin. Also updates critters and prerender-loader and drops prerender cruft.

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,6 @@
 declare module '@webcomponents/custom-elements';
 
-if (typeof PRERENDER === 'boolean') {
-  // @ts-ignore
-  window.customElements = {
-    define() {},
-  };
-  // @ts-ignore
-  window.MessagePort = function () {};
-  // @ts-ignore
-  window.matchMedia = function () {
-    return { addListener() {} };
-  };
-  require('./init-app.tsx');
-} else if (!('customElements' in self)) {
+if (!('customElements' in self)) {
   import('@webcomponents/custom-elements').then(() => {
     require('./init-app.tsx');
   });
